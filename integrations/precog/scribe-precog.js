d01eb79847bfc5aaee225543eab15cb5cbb91f5a
John A. De Goes <john@degoes.net>
2013-05-21 19:22:11 -0600

    no op implementation of precog tracker

---
+++ integrations/precog/scribe-precog.js
@@ -0,0 +1,12 @@
+var ScribePrecogTracker = function(config) {
+  if (!(this instanceof ScribePrecogTracker)) return new ScribePrecogTracker(config);
+
+  this.config = config;
+};
+
+ScribePrecogTracker.prototype.tracker = function(opts) {
+  var path = opts.path;
+  var value = opts.value;
+
+  
+};
