6db0b22aff5561eac98eaaae1504ca9e1da4b51e
John A. De Goes <john@degoes.net>
2013-05-24 08:23:41 -0600

    vastly improved link tracking

--- src/scribe-analytics.js
+++ src/scribe-analytics.js
@@ -534,6 +534,55 @@ if (typeof Scribe === 'undefined') {
       return result;
     };
 
+    var DomUtil = {};
+
+    DomUtil.simulateMouseEvent = function(element, eventName, options) {
+      var eventMatchers = {
+        'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+        'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+      };
+ 
+      options = Util.merge({
+        pointerX: 0,
+        pointerY: 0,
+        button: 0,
+        ctrlKey: false,
+        altKey: false,
+        shiftKey: false,
+        metaKey: false,
+        bubbles: true,
+        cancelable: true
+      }, options || {});
+
+      var oEvent, eventType = null;
+
+      for (var name in eventMatchers) {
+        if (eventMatchers[name].test(eventName)) { eventType = name; break; }
+      }
+
+      if (!eventType) throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+      if (document.createEvent) {
+        oEvent = document.createEvent(eventType);
+        if (eventType === 'HTMLEvents') {
+          oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+        } else {
+          oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+            options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+            options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element
+          );
+        }
+        element.dispatchEvent(oEvent);
+      } else {
+        options.clientX = options.pointerX;
+        options.clientY = options.pointerY;
+        var evt = document.createEventObject();
+        oEvent = Util.merge(evt, options);
+        element.fireEvent('on' + eventName, oEvent);
+      }
+      return element;
+    };
+
     var ArrayUtil = {};
 
     ArrayUtil.exists = function(array, f) {
@@ -947,14 +996,26 @@ if (typeof Scribe === 'undefined') {
       // Track all clicks on links:
       Util.monitorElements('a', function(el) {
         Events.onevent(el, 'click', true, function(e) {
-          e.preventDefault();
+          var target = e.target;
 
-          self.track('click', 
-            {target: Util.merge({url: Util.parseUrl(el.href)}, Util.getNodeDescriptor(e.target))},
-            function() {
-              // Redirect to the link location:
-              document.location = el.href;
-            });
+          var intercepted = target.getAttribute('scribe_intercepted');
+
+          if (!intercepted) {
+            target.setAttribute('scribe_intercepted', 'true');
+
+            e.preventDefault();
+
+            self.track('click', 
+              {target: Util.merge({url: Util.parseUrl(el.href)}, Util.getNodeDescriptor(target))},
+              // TODO: Push to outbox if necessary; improve latency for in-site links.
+              function() {
+                // Simulate a click to the original element:
+                DomUtil.simulateMouseEvent(target, 'click');
+              }
+            );
+          } else {
+            target.removeAttribute('scribe_intercepted');
+          }
         });
       });
     };
