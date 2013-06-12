4731898c6df2ffd48c39e0f469f6fcea672b0819
John A. De Goes <john@degoes.net>
2013-06-12 10:59:41 -0600

    fix IE issue with missing target

--- src/scribe-analytics.js
+++ src/scribe-analytics.js
@@ -816,7 +816,7 @@ if (typeof Scribe === 'undefined') {
           e.charCode  = (typeof e.which === "number") ? e.which : e.keyCode;
           e.timeStamp = e.timeStamp || (new Date()).getTime();
 
-          if (e.target.nodeType == 3) e.target = e.target.parentNode;
+          if (e.target && e.target.nodeType == 3) e.target = e.target.parentNode;
 
           return f(e);
         };
